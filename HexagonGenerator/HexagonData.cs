82ee75d588a9dedb111ab391f2d1b12ced174466
Stephan Sum <sum@live.de>
2016-10-18 13:52:14 +0200

    save

---
+++ HexagonGenerator/HexagonData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexagonGenerator
+{
+    public class HexagonData
+    {
+        public int RowIndex { get; private set; }
+        public int ColIndex { get; private set; }
+        public double DistanceToMainAxis { get; private set; }
+
+
+        public HexagonData(int rowIndex, int colIndex, double distanceToMainAxis)
+        {
+            RowIndex = rowIndex;
+            ColIndex = colIndex;
+            DistanceToMainAxis = distanceToMainAxis;
+        }
+    }
+}
--- HexagonGenerator/MainWindow.xaml.cs
+++ HexagonGenerator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,13 +19,60 @@ namespace HexagonGenerator
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
 
         private Canvas _canvas;
         private double _hexagonSideLength = 10.0;
         private int _hexagonSideCount = 10;
-        private int _oxigenReducerCount = 40;
+
+        private int _oxigenReducerCount = 60;
+        public int OxigenReducerCount
+        {
+            get { return _oxigenReducerCount; }
+            set {
+                _oxigenReducerCount = value;
+                Notify("OxigenReducerCount");
+            }
+        }
+
+        private int _oxigenGainerCount = 40;
+        public int OxigenGainerCount
+        {
+            get { return _oxigenGainerCount; }
+            set
+            {
+                _oxigenGainerCount = value;
+                Notify("OxigenGainerCount");
+            }
+        }
+
+
+        private double _oxigenReducerVariance = 0.8;
+        public double OxigenReducerVariance
+        {
+            get { return _oxigenReducerVariance; }
+            set
+            {
+                _oxigenReducerVariance = value;
+                Notify("OxigenReducerVariance");
+            }
+        }
+
+        private double _oxigenGainerVariance = 1.0;
+        public double OxigenGainerVariance
+        {
+            get { return _oxigenGainerVariance; }
+            set
+            {
+                _oxigenGainerVariance = value;
+                Notify("OxigenGainerVariance");
+            }
+        }
+
+
+
+
 
         private int _hexagonMaxLengthCount { get { return 2 * _hexagonSideCount - 1; } }
 
@@ -36,7 +84,7 @@ namespace HexagonGenerator
 
             DrawGrid();
 
-            DistributeOxigenReducers();
+            GenerateMap_Click(null, null);
 
         }
 
@@ -47,10 +95,12 @@ namespace HexagonGenerator
             _canvas = new Canvas();
             _canvas.Width = canvasLength;
             _canvas.Height = canvasLength * (Math.Sqrt(3.0) / 2.0);
-            //_canvas.Background = new SolidColorBrush(Colors.LightGray);
+            //_canvas.HorizontalAlignment = HorizontalAlignment.Stretch;
+            //_canvas.VerticalAlignment = VerticalAlignment.Stretch;
+            //_canvas.Background = new SolidColorBrush(Colors.Red);
             _canvas.Margin = new Thickness(10.0);
 
-            Panel.Children.Add(_canvas);
+            ViewBox.Child = _canvas;
         }
 
         public void DrawPoint(double x, double y)
@@ -66,7 +116,7 @@ namespace HexagonGenerator
             _canvas.Children.Add(ellipse);
         }
 
-        public void DrawHexagonAbsolute(double x, double y, double distanceToMainAxis)
+        public void DrawHexagonAbsolute(double x, double y, HexagonData data)
         {
             Polygon hexagon = new Polygon();
             hexagon.Stroke = Brushes.Black;
@@ -81,7 +131,9 @@ namespace HexagonGenerator
 
             };
 
-            hexagon.DataContext = CalculateProbabilityFactor(distanceToMainAxis);
+            
+
+            hexagon.DataContext = data;
 
             Canvas.SetLeft(hexagon, x);
             Canvas.SetTop(hexagon, y);
@@ -89,28 +141,64 @@ namespace HexagonGenerator
             _canvas.Children.Add(hexagon);
         }
 
-        public double CalculateProbabilityFactor(double distanceToMainAxis)
+        public double CalculateProbabilityFactor(double variance, double distanceToMainAxis)
         {
-            var variance = 1.0;
-            return (1 / (variance * Math.Sqrt(2 * Math.PI))) * Math.Pow(Math.E, -0.5 * Math.Pow(distanceToMainAxis / variance, 2.0));
+            return (1.0 / (variance * Math.Sqrt(2.0 * Math.PI))) * Math.Pow(Math.E, -0.5 * Math.Pow(distanceToMainAxis / variance, 2.0));
         }
 
+
+        Dictionary<Polygon, double> OxigenReducers = new Dictionary<Polygon, double>();
+
         public void DistributeOxigenReducers()
+        {
+            OxigenReducers.Clear();
+
+            var rnd = new Random();
+            
+            foreach (Polygon hexagon in _canvas.Children)
+            {
+                var hexagonData = hexagon.DataContext as HexagonData;
+                if (! (hexagonData.RowIndex == Math.Floor(_hexagonMaxLengthCount / 2.0) && 
+                    (hexagonData.ColIndex == 0 || (hexagonData.ColIndex == (_hexagonMaxLengthCount-1)))))
+                {
+                    var probability = CalculateProbabilityFactor(OxigenReducerVariance, hexagonData.DistanceToMainAxis);
+                    var score = rnd.Next(1, 10) * (1.0 + probability);
+                    OxigenReducers.Add(hexagon, score);
+                }
+            }
+
+            OxigenReducers = OxigenReducers.OrderByDescending(pair => pair.Value).Take(_oxigenReducerCount).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var reducer in OxigenReducers)
+            {
+                var hexagon = reducer.Key;
+                hexagon.Fill = Brushes.Red;
+            }
+        }
+
+        public void DistributeOxigenGainers()
         {
             var rnd = new Random();
             var results = new Dictionary<Polygon, double>();
             foreach (Polygon hexagon in _canvas.Children)
             {
-                var score = rnd.Next(1, 10) * (1 + (double)hexagon.DataContext);
-                results.Add(hexagon, score);
+                var hexagonData = hexagon.DataContext as HexagonData;
+                if (hexagonData.RowIndex != Math.Floor(_hexagonMaxLengthCount / 2.0) &&
+                    !(hexagonData.ColIndex == 0 || hexagonData.ColIndex == _hexagonMaxLengthCount - 1) && !OxigenReducers.ContainsKey(hexagon))
+                {
+                    var probability = CalculateProbabilityFactor(OxigenGainerVariance, hexagonData.DistanceToMainAxis);
+                    var score = rnd.Next(1, 10) * (1.0 - probability);
+                    results.Add(hexagon, score);
+                }
             }
 
-            foreach (var result in results.OrderByDescending(pair => pair.Value).Take(_oxigenReducerCount))
+            foreach (var result in results.OrderByDescending(pair => pair.Value).Take(_oxigenGainerCount))
             {
                 var hexagon = result.Key;
-                hexagon.Fill = Brushes.Red;
+                hexagon.Fill = Brushes.Green;
             }
         }
+
 
         public void DrawHexagonOnGrid(int rowIndex, int colIndex)
         {
@@ -126,9 +214,11 @@ namespace HexagonGenerator
 
             double y = _hexagonSideLength + rowIndex * 1.5 * _hexagonSideLength;
 
-            var distanceToMainAxis = Math.Abs(Math.Round(_hexagonMaxLengthCount / 2.0, MidpointRounding.ToEven) - rowIndex);
+            var distanceToMainAxis = Math.Abs(Math.Floor(_hexagonMaxLengthCount / 2.0) - rowIndex);
 
-            DrawHexagonAbsolute(x - ((_hexagonSideLength * Math.Sqrt(3.0)) / 2.0), y - _hexagonSideLength, distanceToMainAxis);
+            var hexagonData = new HexagonData(rowIndex, colIndex, distanceToMainAxis);
+
+            DrawHexagonAbsolute(x - ((_hexagonSideLength * Math.Sqrt(3.0)) / 2.0), y - _hexagonSideLength, hexagonData);
         }
         
 
@@ -165,10 +255,47 @@ namespace HexagonGenerator
             }
         }
 
-        private void GenerateOxigenReducers_Click(object sender, RoutedEventArgs e)
+        private void GenerateMap_Click(object sender, RoutedEventArgs e)
         {
             Reset();
             DistributeOxigenReducers();
+            DistributeOxigenGainers();
+        }
+
+        //private void SaveAsImage_Click(object sender, RoutedEventArgs e)
+        //{
+
+
+        //    RenderTargetBitmap rtb = new RenderTargetBitmap((int)_canvas.RenderSize.Width,
+        //    (int)_canvas.RenderSize.Height, 96d, 96d, System.Windows.Media.PixelFormats.Default);
+        //    rtb.Render(_canvas);
+
+        //    var crop = new CroppedBitmap(rtb, new Int32Rect(0, 0, (int)_canvas.RenderSize.Width, (int)_canvas.RenderSize.Height));
+
+        //    BitmapEncoder pngEncoder = new PngBitmapEncoder();
+        //    pngEncoder.Frames.Add(BitmapFrame.Create(crop));
+
+        //    using (var fs = System.IO.File.OpenWrite("canvas.png"))
+        //    {
+        //        pngEncoder.Save(fs);
+        //    }
+        //}
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Notify(string argument)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(argument));
+            }
         }
     }
 }
